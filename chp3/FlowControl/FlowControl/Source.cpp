00b05f0a5fdda7a67018f9f6ad6a0ef17305d47a
Tochukwu Nwachukwu <truetochukz@gmail.com>
2021-07-07 23:44:20 +0200

    control flow

---
+++ chp3/FlowControl/FlowControl/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 5;
+	int y = 4;
+
+	cout << "x == y is " << boolalpha << (x == y) << endl;
+ }
+
+/**
+  Output 
+    x == y is false
+*/
